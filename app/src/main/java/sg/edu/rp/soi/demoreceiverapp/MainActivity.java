407611f5412cebf37149b0d88fc093bd774ee8cb
18016638 <18016638@myrp.edu.sg>
2020-08-20 11:55:37 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/soi/demoreceiverapp/MainActivity.java
@@ -0,0 +1,23 @@
+package sg.edu.rp.soi.demoreceiverapp;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    private BroadcastReceiver br;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        br = new BatteryReceiver();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction("android.intent.action.BATTERY_LOW");
+        this.registerReceiver(br,filter);
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        this.unregisterReceiver(br);
+    }
+}
